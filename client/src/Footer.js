e366523701066dba4b7b356e00a0642123655522
Neel-Shah-29 <84740927+Neel-Shah-29@users.noreply.github.com>
2022-02-13 01:00:21 +0530

    Delete client directory

--- client/src/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-    return (
-        <div class="footer">
-            <p className="text-center">Copyright &copy; Sociablast.com</p>
-        </div>
-    )
-}
